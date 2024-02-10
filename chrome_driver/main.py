aa693238ba942e31156507a46ac8dbaae9d10b1b
Nikolay Kovyrshin <nikolaykovyrshin@outlook.com>
2024-02-10 16:33:28 +0300

    Hide text with "autotest"

--- chrome_driver/main.py
+++ chrome_driver/main.py
@@ -6,6 +6,10 @@ import time
 def auto_check():
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     url = "https://www.ironspider.ca/forms/checkradio.htm"
